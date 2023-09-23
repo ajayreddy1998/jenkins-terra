730f258e372dab7d24654eed270f93b9d2f109d7
ajayreddy1998 <69149645+ajayreddy1998@users.noreply.github.com>
2023-09-23 18:20:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,14 +3,6 @@ provider "aws" {
    profile = "default" 
 }
 
-variable "ami" {
-  description = "this is to select the ami option "
-}
-
-variable "instance_type" {
- description = "this is for the type of instance "
-
-}
  resource "aws_instance" "ec-2" {
    ami = "ami-053b0d53c279acc90"
    instance_type = "t2.micro"
