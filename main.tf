a2933ade1df91f30d351b0f7aa9a982a8151b29d
ajayreddy1998 <69149645+ajayreddy1998@users.noreply.github.com>
2023-09-24 21:43:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,16 +8,35 @@ required_providers {
 
   backend "s3" { 
      bucket = "terraform-ajay-state"
-     key = "ec-2_s3"
+     key = "ec-2_s32"
      region = "us-east-1"
    }
 }
+
 provider "aws" {
-   region = "us-east-1"
+ region = "us-east-1"
 }
 
- resource "aws_instance" "ec-2" {
-   ami = "ami-053b0d53c279acc90"
-   instance_type = "t2.micro"
-   key_name = "linux"
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  key_name = "linux"
+}
+
+provisioner "remote-exec" {
+  inline = [
+    "sudo su "
+    "apt-get install nginx"
+    "systemctl start nginx"
+    "mkdir page"
+    "git pull https://github.com/ajayreddy1998/nonprofitwebsite.git"
+    "cp -r page /var/www/html/"
+  ]
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = "linux"
+    host        = aws_instance.example.public_ip
+  }
 }
