0331dcb21b88ca0fffddad815782aba95e98d581
ajayreddy1998 <69149645+ajayreddy1998@users.noreply.github.com>
2023-09-27 07:02:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "aws_vpc" "myvpc" {
 
 resource "aws_key_pair" "ajay" {
   key_name = "sample"
-  public_key = file("/home/ubuntu/sam.pub")
+  public_key = file("sudo /home/ubuntu/sam.pub")
 }
 
 resource "aws_subnet" "sub1" {
@@ -91,8 +91,8 @@ resource "aws_instance" "example" {
   connection {
     type = "ssh"
     user = "ubuntu"
-    //private_key = file("/home/ubuntu/sam")
-    private_key = "linux.pem"
+    private_key = file("sudo /home/ubuntu/sam")
+    //private_key = "linux.pem"
     host = self.public_ip
   }
 
