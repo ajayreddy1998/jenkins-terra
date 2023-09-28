2e54399ed24770e195c154b7226dfe17e6389a2c
ajayreddy1998 <69149645+ajayreddy1998@users.noreply.github.com>
2023-09-29 00:30:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -112,7 +112,7 @@ resource "aws_instance" "example" {
       "sudo mkdir -p /etc/apt/keyrings",
       "curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg",
       //NODE_MAJOR=20
-      "echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list",
+      //"echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list",
       "npm install",
       "npm run build",
       "cd build",
