26d47f9647287543d3188c0d35135fd88f42920e
ajayreddy1998 <69149645+ajayreddy1998@users.noreply.github.com>
2023-09-29 07:19:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -104,19 +104,19 @@ resource "aws_instance" "example" {
       //"sudo apt-get install -y npm",
       //"mkdir code",
       //"cd code",
-      //"git clone https://github.com/ajayreddy1998/Yeshlaptop.git",
-      "git clone https://github.com/ajayreddy1998/microservice.git",
-      "cd microservices/coit-frontend",
+      "git clone https://github.com/ajayreddy1998/Yeshlaptop.git",
+      //"git clone https://github.com/ajayreddy1998/microservice.git",
+      //"cd microservices/coit-frontend",
       //sudo apt-get update
-      "sudo apt-get install -y ca-certificates curl gnupg",
-      "sudo mkdir -p /etc/apt/keyrings",
-      "curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg",
+     // "sudo apt-get install -y ca-certificates curl gnupg",
+      //"sudo mkdir -p /etc/apt/keyrings",
+      //"curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg",
       //NODE_MAJOR=20
       //"echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list",
-      "npm install",
-      "npm run build",
-      "cd build",
-      "sudo cp -r * /var/www/html", 
+      //"npm install",
+      //"npm run build",
+      //"cd",
+      "sudo cp index.html /var/www/html", 
      ]
   }
 }
